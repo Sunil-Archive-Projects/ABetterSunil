02e1b4cb45ceda20bbc19fdeb326226ca6f1a814
Sunil L <sunilandroidnayak@gmail.com>
2022-08-10 23:11:27 +0530

    add : 10 aug data

---
+++ data/bookReading.js
@@ -0,0 +1,14 @@
+const bookReading_data = {
+    2022: {
+      August: {
+        6: { books : [{title:"", chapters:""}]  },
+        7: {  books : [{title:"", chapters:""}] },
+        8: {  books : [{title:"Thinking, Fast and Slow: Kahneman, Daniel", chapters:"1"}] },
+        9: { books : [{title:"Thinking, Fast and Slow: Kahneman, Daniel", chapters:"2"}] },
+        10: { books : [{title:"Thinking, Fast and Slow: Kahneman, Daniel", chapters:"2"}]},
+        11: { books : [{title:"", chapters:""}]}
+      },
+      September: {},
+    },
+  };
+  
--- data/workout.js
+++ data/workout.js
@@ -1,6 +1,13 @@
 const workout_data = {
-  2022 : {
-  August:{1:{}, 2:{},3:{},4:{},5:{}},
-  September:{}
-  }
-}
+  2022: {
+    August: {
+      6: { jumpRopes: 0, strengthTraining: 0, cycling : 0, water:3 },
+      7: { jumpRopes: 1500, strengthTraining: 30, cycling:4,  water:3 },
+      8: { jumpRopes: 1500,  strengthTraining: 30, cycling:10,  water:3 },
+      9: { jumpRopes: 1500, strengthTraining:0, cycling:2, water:3 },
+      10: { jumpRopes: 1500, strengthTraining:0, cycling:0, water:3 },
+      11: { jumpRopes: 0, strengthTraining:0, cycling:0, water:0 },
+    },
+    September: {},
+  },
+};
