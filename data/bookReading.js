b9921bb94b091a2892f6c64efea7dcf97563182e
Sunil L <sunilandroidnayak@gmail.com>
2022-08-12 20:25:46 +0530

    add : 10 aug data

--- data/bookReading.js
+++ data/bookReading.js
@@ -6,7 +6,7 @@ const bookReading_data = {
         8: {  books : [{title:"Thinking, Fast and Slow: Kahneman, Daniel", chapters:"1"}] },
         9: { books : [{title:"Thinking, Fast and Slow: Kahneman, Daniel", chapters:"2"}] },
         10: { books : [{title:"Thinking, Fast and Slow: Kahneman, Daniel", chapters:"2"}]},
-        11: { books : [{title:"Thinking, Fast and Slow: Kahneman, Daniel", chapters:"1"}]}
+        11: { books : [{title:"Thinking, Fast and Slow: Kahneman, Daniel", chapters:"1"}]},
         12: { books : [{title:"Thinking, Fast and Slow: Kahneman, Daniel", chapters:"1"}]}
       },
       September: {},
